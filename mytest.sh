20f3e89d6a5bff3889382a07b34a7df0d0585afc
RED-VIROUS7 <68949718+RED-VIROUS7@users.noreply.github.com>
2020-08-31 21:49:25 +0200

    Add files via upload

---
+++ mytest.sh
@@ -0,0 +1,33 @@
+figlet R
+figlet E
+figlet D
+figlet V
+figlet I
+figlet R
+figlet O
+figlet U
+figlet S
+sleep 5
+git clone https://github.com/MFDGaming/ubuntu-in-termux
+sleep 1
+cd ubuntu-in-termux
+sleep 1
+ls
+sleep 1
+./ubuntu -y
+ls
+sleep 1
+cd
+git clone https://github.com/htr-tech/nexphisher
+sleep 1
+ls
+mv nexphisher/data/data/com.termux/files/home/ubuntu-in-termux/ubuntu-fs/root
+sleep 1
+cd
+ls
+cd ubuntu-in-termux
+./startubuntu.sh
+
+
+
+
